a35ddd52b1a97585cdc8f5b73836ce591eb0f993
Ryan Shin <1982sdr@hanmail.net>
2016-02-12 16:21:49 +0900

    TDD 9장. 우리가 사는 시간

--- src/kr/co/tdd/Dollar.java
+++ src/kr/co/tdd/Dollar.java
@@ -1,10 +1,10 @@
 package kr.co.tdd;
 
 public class Dollar extends Money{
-	Dollar(int amount) {
-		this.amount = amount;
+	Dollar(int amount, String currency) {
+		super(amount, currency);
 	}
 	public Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
 }
--- src/kr/co/tdd/Money.java
+++ src/kr/co/tdd/Money.java
@@ -2,16 +2,24 @@ package kr.co.tdd;
 
 public abstract class Money {
 	protected int amount;
+	protected String currency;
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount
 			&& getClass().equals(money.getClass());
 	}
 	public static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 	public abstract Money times(int multiplier);
+	public String currency() {
+		return currency;
+	}
 }
